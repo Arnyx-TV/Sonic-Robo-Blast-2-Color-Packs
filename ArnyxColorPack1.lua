51ddd226325cd62d70c118cce066010560ab6292
Arnyx <72172608+Arnyx-TV@users.noreply.github.com>
2020-09-30 19:42:31 -0400

    Color Pack 1
    The first color pack was created the day v2.2.6 released, on July 10th 2020. The last color (Falling Blue) was made August 13th 2020.

---
+++ ArnyxColorPack1.lua
@@ -0,0 +1,144 @@
+freeslot("SKINCOLOR_FDRED")
+skincolors[SKINCOLOR_FDRED] = {
+	name = "FD Red",
+	ramp = {33,214,42,42,43,44,45,46,71,47,47,47,30,30,31,31},
+	invcolor = SKINCOLOR_BLUE,
+	invshade = 12,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_CPOLDBLUE")
+skincolors[SKINCOLOR_CPOLDBLUE] = {
+	name = "CP Old Blue",
+	ramp = {0,145,148,149,150,150,172,173,173,174,175,175,139,139,254,31},
+	invcolor = SKINCOLOR_RED,
+	invshade = 9,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_PURIN")
+skincolors[SKINCOLOR_PURIN] = {
+	name = "Purin",
+	ramp = {0,1,80,81,82,84,64,66,58,68,69,79,237,239,29,31},
+	invcolor = SKINCOLOR_PURPLE,
+	invshade = 9,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_LEMONADE")
+skincolors[SKINCOLOR_LEMONADE] = {
+	name = "Lemonade",
+	ramp = {0,1,80,80,81,81,82,83,64,74,75,76,78,79,239,31},
+	invcolor = SKINCOLOR_GREEN,
+	invshade = 9,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_PIZZA")
+skincolors[SKINCOLOR_PIZZA] = {
+	name = "Pizza",
+	ramp = {0,84,74,54,56,37,37,38,40,41,43,45,46,47,30,31},
+	invcolor = SKINCOLOR_ORANGE,
+	invshade = 9,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_MINTCHOCOLATE")
+skincolors[SKINCOLOR_MINTCHOCOLATE] = {
+	name = "Mint Chocolate",
+	ramp = {0,3,88,121,96,97,112,113,104,79,70,46,47,47,30,31},
+	invcolor = SKINCOLOR_GREEN,
+	invshade = 9,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_GREENEGGS")
+skincolors[SKINCOLOR_GREENEGGS] = {
+	name = "Green Eggs",
+	ramp = {0,0,4,90,100,104,116,117,117,117,118,118,119,111,111,31},
+	invcolor = SKINCOLOR_GREEN,
+	invshade = 9,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_ROSE")
+skincolors[SKINCOLOR_ROSE] = {
+	name = "Rose",
+	ramp = {0,13,213,215,39,39,40,41,43,44,45,46,47,47,30,31},
+	invcolor = SKINCOLOR_BLUE,
+	invshade = 9,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_IZUNAGI")
+skincolors[SKINCOLOR_IZUNAGI] = {
+	name = "Izunagi",
+	ramp = {0,3,5,8,11,16,22,23,24,25,26,26,27,28,30,31},
+	invcolor = SKINCOLOR_YELLOW,
+	invshade = 9,
+	chatcolor = V_WHITEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_FDDARKBLUE")
+skincolors[SKINCOLOR_FDDARKBLUE] = {
+	name = "FD Dark Blue",
+	ramp = {0,170,155,155,156,157,157,158,158,159,159,253,254,254,30,31},
+	invcolor = SKINCOLOR_RED,
+	invshade = 9,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_ARMYGREEN")
+skincolors[SKINCOLOR_ARMYGREEN] = {
+	name = "Army Green",
+	ramp = {0,5,90,90,90,85,86,242,243,245,247,250,24,27,29,31},
+	invcolor = SKINCOLOR_GREEN,
+	invshade = 9,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_MONSTROSITY")
+skincolors[SKINCOLOR_MONSTROSITY] = {
+	name = "A Monstrosity LMAO",
+	ramp = {35,151,73,182,54,112,255,165,70,2,199,33,96,130,173,31},
+	invcolor = SKINCOLOR_GREEN,
+	invshade = 9,
+	chatcolor = V_BLACKMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_IMPERFECTORANGE")
+skincolors[SKINCOLOR_IMPERFECTORANGE] = {
+	name = "Imperfect Orange",
+	ramp = {0,4,7,86,224,225,215,215,39,40,42,44,71,47,30,31},
+	invcolor = SKINCOLOR_BLUE,
+	invshade = 9,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_VIBINGDETECTIVE")
+skincolors[SKINCOLOR_VIBINGDETECTIVE] = {
+	name = "Vibing Detective",
+	ramp = {148,172,172,172,172,173,173,174,174,175,175,139,139,254,30,31},
+	invcolor = SKINCOLOR_YELLOW,
+	invshade = 9,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_GRADIANTMONK")
+skincolors[SKINCOLOR_GRADIANTMONK] = {
+	name = "Gradiant Monk",
+	ramp = {0,160,161,162,163,166,166,195,195,195,225,224,66,75,74,31},
+	invcolor = SKINCOLOR_PURPLE,
+	invshade = 9,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_FALLINGBLUE")
+skincolors[SKINCOLOR_FALLINGBLUE] = {
+	name = "Falling Blue",
+	ramp = {0,145,132,135,135,148,163,194,195,195,196,197,198,199,29,31},
+	invcolor = SKINCOLOR_YELLOW,
+	invshade = 9,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
