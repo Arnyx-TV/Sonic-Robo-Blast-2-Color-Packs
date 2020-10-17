384547c8ddf7fcc569b96cd3e61a1c90f3d99e68
Arnyx <72172608+Arnyx-TV@users.noreply.github.com>
2020-10-17 12:40:12 -0400

    Color Pack 1.5
    3 more colors to choose for Multiplayer.
    
    Sunset Green
    Mr. Blue Sky
    Bright Plum

---
+++ ArnyxColorPack1-5.lua
@@ -0,0 +1,27 @@
+freeslot("SKINCOLOR_SUNSETGRASS")
+skincolors[SKINCOLOR_SUNSETGRASS] = {
+	name = "Sunset Grass",
+	ramp = {0,88,89,65,190,190,191,191,104,105,116,117,118,119,111,31},
+	invcolor = SKINCOLOR_ORANGE,
+	invshade = 12,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_MRBLUESKY")
+skincolors[SKINCOLOR_MRBLUESKY] = {
+	name = "Mr. Blue Sky",
+	ramp = {0,129,255,255,255,255,134,134,135,136,136,137,138,139,139,31},
+	invcolor = SKINCOLOR_RED,
+	invshade = 12,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+freeslot("SKINCOLOR_BRIGHTPLUM")
+skincolors[SKINCOLOR_BRIGHTPLUM] = {
+	name = "Bright Plum",
+	ramp = {0,144,145,145,170,170,162,163,163,164,165,167,168,169,254,31},
+	invcolor = SKINCOLOR_BLUE,
+	invshade = 12,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
